b40ac632f8457759c7275a71b37bcf1f06114411
agmarchena@corenetworks.com <agmarchena@corenetworks.com>
2023-04-20 21:05:29 +0200

    constructores

--- src/main/java/es/corenetwors/dam/programacion/tema4/constructores/Rueda.java
+++ src/main/java/es/corenetwors/dam/programacion/tema4/constructores/Rueda.java
@@ -2,4 +2,18 @@ package es.corenetwors.dam.programacion.tema4.constructores;
 
 public class Rueda {
 
+	private int radio;
+	private int tipo;
+	
+	
+	
+	public Rueda(int radio, int tipo) {
+		super();
+		this.radio = radio;
+		this.tipo = tipo;
+	}
+	
+	
+	
+	
 }
